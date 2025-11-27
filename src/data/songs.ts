12dbe2d3d73ec9d6e097adaa2e251550c12feafb
sarveshmariappan <sarveshmariappan2005@gmail.com>
2025-11-27 19:20:09 +0530

    Add

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -13,79 +13,79 @@ export const tamilSongs: Song[] = [
     title: "Kannalane",
     artist: "A.R. Rahman",
     album: "Bombay",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
-    coverImage: "https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/kannalane.mp3",
+    coverImage: "/images/kannalane.jpg"
   },
   {
     id: 2,
     title: "Munbe Vaa",
     artist: "A.R. Rahman",
     album: "Sillunu Oru Kaadhal",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
-    coverImage: "https://images.pexels.com/photos/167636/pexels-photo-167636.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/munbe-vaa.mp3",
+    coverImage: "/images/munbe-vaa.jpg"
   },
   {
     id: 3,
     title: "Kadhal Rojave",
     artist: "A.R. Rahman",
     album: "Roja",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
-    coverImage: "https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/kadhal-rojave.mp3",
+    coverImage: "/images/kadhal-rojave.jpg"
   },
   {
     id: 4,
     title: "Vennilave",
     artist: "Ilaiyaraaja",
     album: "Minsara Kanavu",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
-    coverImage: "https://images.pexels.com/photos/1540406/pexels-photo-1540406.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/vennilave.mp3",
+    coverImage: "/images/vennilave.jpg"
   },
   {
     id: 5,
     title: "Uyire",
     artist: "A.R. Rahman",
     album: "Bombay",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3",
-    coverImage: "https://images.pexels.com/photos/1327430/pexels-photo-1327430.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/uyire.mp3",
+    coverImage: "/images/uyire.jpg"
   },
   {
     id: 6,
     title: "Why This Kolaveri Di",
     artist: "Anirudh Ravichander",
     album: "3",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3",
-    coverImage: "https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/why-this-kolaveri-di.mp3",
+    coverImage: "/images/why-this-kolaveri-di.jpg"
   },
   {
     id: 7,
     title: "Thalli Pogathey",
     artist: "A.R. Rahman",
     album: "Achcham Yenbadhu Madamaiyada",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3",
-    coverImage: "https://images.pexels.com/photos/1308624/pexels-photo-1308624.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/thalli-pogathey.mp3",
+    coverImage: "/images/thalli-pogathey.jpg"
   },
   {
     id: 8,
     title: "Rowdy Baby",
     artist: "Yuvan Shankar Raja",
     album: "Maari 2",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3",
-    coverImage: "https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/rowdy-baby.mp3",
+    coverImage: "/images/rowdy-baby.jpg"
   },
   {
     id: 9,
     title: "Nenjukkul Peidhidum",
     artist: "Yuvan Shankar Raja",
     album: "Vaaranam Aayiram",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3",
-    coverImage: "https://images.pexels.com/photos/1576937/pexels-photo-1576937.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/nenjukkul-peidhidum.mp3",
+    coverImage: "/images/nenjukkul-peidhidum.jpg"
   },
   {
     id: 10,
     title: "Vaseegara",
     artist: "Harris Jayaraj",
     album: "Minnale",
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3",
-    coverImage: "https://images.pexels.com/photos/1047442/pexels-photo-1047442.jpeg?auto=compress&cs=tinysrgb&w=400"
+    audioUrl: "/music/vaseegara.mp3",
+    coverImage: "/images/vaseegara.jpg"
   }
 ];
